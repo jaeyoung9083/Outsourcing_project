9e0fb9f79ac5a7285f16d9fbc160636e93413246
jaeyoung9083 <haeyoung9083@gmail.com>
2023-07-18 15:45:17 +0900

    signup 전 Login, 경로설정 아직 x

--- src/components/LoginPage/Login.jsx
+++ src/components/LoginPage/Login.jsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import { async } from '@firebase/util';
+import { onAuthStateChanged } from 'firebase/auth';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function Login() {
@@ -7,11 +9,47 @@ function Login() {
 
   const navigate = useNavigate();
 
+  const signIn = async (event) => {
+    event.preventDefault();
+    if (!(Email || Pw)) {
+      return alert('아이디와 비밀번호 모두 입력해주세요');
+    }
+    try {
+      !(Email || Pw);
+      userCredential = await signInWithEmailAndPassword(auth, Email, Pw);
+      navigate('/');
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log('error with signIn', errorMessage, errorCode);
+    }
+    if (errorCode === 'auth/user-not-found') {
+      alert('존재하지 않는 이메일입니다');
+    } else if (errorCode === 'auth/user-not-found');
+    {
+      alert('비밀번호가 일치하지 않습니다');
+    }
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auto, (user) => {
+      console.log('user', user);
+    });
+    // auto.currentUser 현재 로그인 중인 유저 정보
+  }, []);
+
+  useEffect(() => {
+    if (user?.userid) {
+      navigate('/');
+    }
+  }, [user, navigate]);
+
   return (
     <div>
       <form>
         <span>로그인</span>
         <div>
+          <label>이메일 : </label>
           <input
             type="email"
             placeholder="이메일을 입력해주세요"
@@ -23,6 +61,7 @@ function Login() {
           />
         </div>
         <div>
+          <label>비밀번호 : </label>
           <input
             type="password"
             placeholder="비밀전호를 입력해주세요"
@@ -34,7 +73,7 @@ function Login() {
           />
         </div>
 
-        <button>로그인</button>
+        <button onClick={signIn}>로그인</button>
 
         <p>회원가입 하로 가기</p>
         <button
