e5e17d470197f3f3d080375fdb8c84bc3c9bcf15
jinny <ertmiu96@gmail.com>
2023-07-18 18:48:48 +0900

    signup

--- src/components/LoginPage/Singup.jsx
+++ src/components/LoginPage/Singup.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { auth } from '../../firebase';
 
 function Singup() {
   // hook
@@ -12,8 +13,68 @@ function Singup() {
   const [pw, setPw] = useState('');
   const [pwCheck, setCheck] = useState('');
 
-  return;
-  <div>d</div>;
-}
+  const [currentUser, setCurrentUser] = useState(null);
 
+  return (
+    <div>
+      <form
+        onSubmit={() => {
+          e.preventDefault();
+          dispatch({
+            type: 'ADD_TODO',
+            payload: {}
+          });
+        }}
+      >
+        <span>회원가입</span>
+        <div>
+          <input
+            type="email"
+            placeholder="이메일을 입력해주세요"
+            value={email}
+            name="email"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            placeholder="비밀번호를 입력해주세요"
+            value={pw}
+            name="password"
+            onChange={(e) => {
+              setPw(e.target.value);
+            }}
+          />
+        </div>
+
+        <button
+          onClick={async () => {
+            try {
+              const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+
+              setCurrentUser(userCredential.user.email);
+            } catch (error) {
+              console.error(error);
+            }
+          }}
+        >
+          회원가입
+        </button>
+
+        <p>로그인하러가기</p>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            navigate('/login');
+          }}
+        >
+          로그인
+        </button>
+      </form>
+    </div>
+  );
+}
 export default Singup;
