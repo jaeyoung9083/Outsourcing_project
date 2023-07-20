3f36cd3d7220038c56b1b40f62147d50c0e687d4
skdbsqls <skdbsqls98@naver.com>
2023-07-20 19:42:49 +0900

    feat: 결과창 댓글 CRUD json으로 구현

--- src/components/LoginPage/Login.jsx
+++ src/components/LoginPage/Login.jsx
@@ -3,12 +3,11 @@ import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../firebase';
-import { unwrapResult } from '@reduxjs/toolkit';
 import { addCurrentUser } from '../../redux/modules/loginSlice'; // 리덕스 슬라이스 파일 경로를 수정
+
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const user = useSelector((state) => state.user);
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
@@ -20,8 +19,6 @@ function Login() {
     try {
       const userCredential = await signInWithEmailAndPassword(auth, email, password);
       const user = userCredential.user;
-      // console.log(userCredential);
-      // console.log(userCredential.user);
       // 리덕스 스토어에 유저 정보 추가
       dispatch(
         addCurrentUser({
@@ -43,11 +40,11 @@ function Login() {
       }
     }
   };
-  useEffect(() => {
-    if (user?.users) {
-      navigate('/');
-    }
-  }, [user, navigate]);
+  // useEffect(() => {
+  //   if (user?.users) {
+  //     navigate('/');
+  //   }
+  // }, [user, navigate]);
   return (
     <div>
       <form>
@@ -91,72 +88,3 @@ function Login() {
   );
 }
 export default Login;
-
-// import React, { useEffect, useState } from 'react';
-// import { Link, useNavigate } from 'react-router-dom';
-// import { auth } from '../../firebase';
-// import { signInWithEmailAndPassword } from 'firebase/auth';
-
-// function Login() {
-//   const navigate = useNavigate();
-//   const [email, setEmail] = useState('');
-//   const [password, setPassword] = useState('');
-
-//   // 이메일과 비밀번호 입력 받기
-//   const onChangeEmail = (event) => {
-//     setEmail(event.target.value);
-//   };
-//   const onChangePassword = (event) => {
-//     setPassword(event.target.value);
-//   };
-
-//   // 로그인 버튼
-//   const LoginButton = async (event) => {
-//     event.preventDefault();
-//     // 입력값 검사
-//     if (!email || !password) {
-//       return alert('아이디와 비밀번호를 모두 입력해주세요');
-//     }
-//     // 로그인
-//     try {
-//       const userCredential = await signInWithEmailAndPassword(auth, email, password);
-//       const user = userCredential.user;
-//       console.log(user);
-//       // 로그인 성공하면 홈으로 이동
-//       navigate('/');
-//     } catch (error) {
-//       const errorCode = error.code;
-//       const errorMessage = error.message;
-//       console.log('error with signIn', errorMessage, errorCode);
-//       if (errorCode === 'auth/user-not-found') {
-//         alert('존재하지 않는 이메일입니다');
-//       } else if (errorCode === 'auth/user-not-found');
-//       {
-//         alert('비밀번호가 일치하지 않습니다');
-//       }
-//     }
-//   };
-
-//   // useEffect(() => {
-//   //   if (user?.userid) {
-//   //     navigate('/');
-//   //   }
-//   // }, [user, navigate]);
-
-//   return (
-//     <div>
-//       <div>
-//         <h2>로그인</h2>
-//         <label>이메일 : </label>
-//         <input type="email" placeholder="이메일을 입력해주세요" value={email} onChange={onChangeEmail} />
-//         <label>비밀번호 : </label>
-//         <input type="password" placeholder="비밀번호를 입력해주세요" value={password} onChange={onChangePassword} />
-//         <button onClick={LoginButton}>로그인</button>
-//       </div>
-//       <h2>회원가입 하러 가기</h2>
-//       <Link to={'/signup'}>회원가입</Link>
-//     </div>
-//   );
-// }
-
-// export default Login;
--- src/components/ResultPage/Video.jsx
+++ src/components/ResultPage/Video.jsx
@@ -1,61 +1,61 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-// function Video() {
-//   const [videos, setVideos] = useState([]);
+function Video() {
+  const [videos, setVideos] = useState([]);
 
-//   useEffect(() => {
-//     // YouTube API 호출을 위한 함수
-//     const fetchVideos = async () => {
-//       try {
-//         const apiKey = 'AIzaSyDjd-3edO32urCc5UVJ9zyADQ9D9g1fON4';
-//         const videoIds = '3L2-JAbXIAM,fQNO7YvNCSw,bMYg56tlnao,D6HH_sxSj0M,hFR3uDZnwDY,'; // 여기에 가져오고 싶은 동영상 ID를 입력하세요
-//         const maxResults = 5;
+  useEffect(() => {
+    // YouTube API 호출을 위한 함수
+    const fetchVideos = async () => {
+      try {
+        const apiKey = 'AIzaSyDjd-3edO32urCc5UVJ9zyADQ9D9g1fON4';
+        const videoIds = '3L2-JAbXIAM,fQNO7YvNCSw,bMYg56tlnao,D6HH_sxSj0M,hFR3uDZnwDY,'; // 여기에 가져오고 싶은 동영상 ID를 입력하세요
+        const maxResults = 5;
 
-//         const response = await axios.get(
-//           `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoIds}&maxResults=${maxResults}&key=${apiKey}`
-//         );
-//         // console.log(response.data.items);
+        const response = await axios.get(
+          `https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${videoIds}&maxResults=${maxResults}&key=${apiKey}`
+        );
+        // console.log(response.data.items);
 
-//         const videoList = response.data.items.map((item) => ({
-//           videoId: item.id,
-//           title: item.snippet.title,
-//           thumbnailUrl: item.snippet.thumbnails.default.url
-//         }));
-//         setVideos(videoList);
-//       } catch (error) {
-//         console.error('Error fetching videos:', error);
-//       }
-//     };
+        const videoList = response.data.items.map((item) => ({
+          videoId: item.id,
+          title: item.snippet.title,
+          thumbnailUrl: item.snippet.thumbnails.default.url
+        }));
+        setVideos(videoList);
+      } catch (error) {
+        console.error('Error fetching videos:', error);
+      }
+    };
 
-//     fetchVideos();
-//   }, []);
+    fetchVideos();
+  }, []);
 
-//   return (
-//     <div
-//       style={{
-//         width: '800px',
-//         overflowX: 'auto', // 가로 스크롤바 생성을 위한 설정
-//         display: 'flex',
-//         gap: '10px',
-//         paddingBottom: '20px'
-//       }}
-//     >
-//       {videos.map((video) => (
-//         <div key={video.videoId}>
-//           <iframe
-//             width="300"
-//             height="165"
-//             src={`https://www.youtube.com/embed/${video.videoId}`}
-//             title={video.title}
-//             frameBorder="0" // 영상 프레임 경계선 없애기
-//             allowFullScreen // 전체화면으로 플레이어 보여주기
-//           ></iframe>
-//           <span>{video.title}</span>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
+  return (
+    <div
+      style={{
+        width: '800px',
+        overflowX: 'auto', // 가로 스크롤바 생성을 위한 설정
+        display: 'flex',
+        gap: '10px',
+        paddingBottom: '20px'
+      }}
+    >
+      {videos.map((video) => (
+        <div key={video.videoId}>
+          <iframe
+            width="300"
+            height="165"
+            src={`https://www.youtube.com/embed/${video.videoId}`}
+            title={video.title}
+            frameBorder="0" // 영상 프레임 경계선 없애기
+            allowFullScreen // 전체화면으로 플레이어 보여주기
+          ></iframe>
+          <span>{video.title}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
 
-// export default Video;
+export default Video;
